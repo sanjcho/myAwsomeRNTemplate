6ca4ea9d1bc7049aad2bcd1c0f878179d9738a20
AAMakarenko <sanjcho@gmail.com>
2020-05-24 05:17:57 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import * as React from 'react';
+import {GreetingScreen} from './screens/GreetingScreen';
+
+const App = () => {
+  return (
+    <GreetingScreen/>
+  );
+};
+
+
+export default App;
---
+++ src/screens/GreetingScreen/index.ts
@@ -0,0 +1,1 @@
+export {GreetingScreen} from './GreetingScreen';
